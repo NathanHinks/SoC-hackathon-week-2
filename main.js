b2e882c20e3ad9733e296f0206fe581251a5490e
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-01-15 10:19:23 +0000

    display question

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+//get the question container
+let question = document.querySelector(".question");
+
+//variables
+let score = 0;
+let questionsRemaining = 10;
+
+function displayQuestion(questionText) {
+    //change the text
+    question.innerText = questionText;
+}
+
+
 async function getData(){
     let response =  await fetch("https://opentdb.com/api.php?amount=5&type=boolean");
     let data = await response.json();
     console.log(data.results[0])
+
+    displayQuestion(data.results[0].question)
 }   
 
 getData();
