4887e79e83f437b462306e5c380acb1b3b8a38a1
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-01-15 20:07:25 +0000

    refactored

--- main.js
+++ main.js
@@ -1,85 +1,83 @@
-//get the question container
-let question = document.querySelector(".question");
+//DOM
+const question = document.querySelector(".question");
+const questionsRemainingBoard = document.querySelector(".questionsRemaining")
+const scoreBoard = document.querySelector(".score")
 
 //variables
+let countdown = 10
 let score = 0;
 let questionsRemaining = 10;
 let currentAnswer = undefined
 
-function displayQuestion(questionText) {
-    //remove random characters (thanks Liam! :D)
+let timer;
+
+
+const  displayQuestion = (questionText) => {
+    //remove random characters
     questionText = questionText.replace(
         /&#039|&rsquo;|&quot;|&#39;|;/g,
         ""
       );
+
     question.innerText = questionText;
 }
 
-
-async function getData(){
-    let response =  await fetch("https://opentdb.com/api.php?amount=1&category=9&type=boolean");
-    let data = await response.json();
-    currentAnswer = data.results[0].correct_answer;
-    displayQuestion(data.results[0].question)
-}   
-
-getData();
-
-
-function compareValue(value){
-    if(value == currentAnswer){
-        score++
-    }
+const compareValue = (value) => {
+    if (value === currentAnswer) score++
+    
     questionsRemaining--
 }
 
-
-function endGame(){
-    //check if score > 5
-    if(score > 5) {
-        alert("you win")
-    }
-    else {
-        alert("you lose, you suck")
-    }
+const endGame = () => {
+    if(score > 5) alert("you win")
+    else alert("you lose")
 }
 
-function resetGame() {
+const resetGame = () => {
     score = 0
     questionsRemaining = 10
 
-    questionsRemainingBoard.innerText = `Questions remaining= ${questionsRemaining}`
-    scoreBoard.innerText = `Score= ${score}`
+    questionsRemainingBoard.innerText = `Questions Remaining: ${questionsRemaining}`
+    scoreBoard.innerText = `Score: ${score}`
+    
     getData()
 }
 
-let questionsRemainingBoard = document.querySelector(".questionsRemaining")
-let scoreBoard = document.querySelector(".score")
-
-
-function buttonClick(button){
-    document.body.style.backgroundImage= "url('https://cdn.images.express.co.uk/img/dynamic/130/590x/Easy-quiz-questions-1282929.webp?r=1610179087464')"
+const buttonClick = (button) => {
     compareValue(button);
+
     questionsRemainingBoard.innerText = `Questions remaining= ${questionsRemaining}`
     scoreBoard.innerText = `Score= ${score}`
+
     getData()
     
     if(score > 5 || questionsRemaining === 0) {
         endGame()
-        //confirm if want to play again.
-        let result = confirm("Play again?")
-        if(result === true) {
-            //reset game
-            resetGame()
-        }
+        
+        const result = confirm("Play again?")
+        if (result === true) resetGame()
     }
 }
 
-let buttonTrue = document.querySelector(".btnTrue");
-buttonTrue.addEventListener("click", function(){buttonClick("True")} )
 
-let buttonFalse = document.querySelector(".btnFalse");
-buttonFalse.addEventListener("click", function() {buttonClick("False")} ) 
+//fetch
+async function getData(){
+    let response =  await fetch("https://opentdb.com/api.php?amount=1&category=9&type=boolean");
+    let data = await response.json();
+    
+    currentAnswer = data.results[0].correct_answer;
+    displayQuestion(data.results[0].question)
+}   
+
+//first call
+getData();
+
+
+const buttonTrue = document.querySelector(".btnTrue");
+buttonTrue.addEventListener("click", () => buttonClick("True"))
+
+const buttonFalse = document.querySelector(".btnFalse");
+buttonFalse.addEventListener("click", () => buttonClick("False")) 
 
 
 
